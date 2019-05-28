458d83ff72b049fe47fa618622bd4d7cce4f0629
jpthiede <john-p-thiede@uiowa.edu>
2019-05-28 16:42:00 -0500

    install.py: editing to support multiple VMs (Work in progress)

--- install.py
+++ install.py
@@ -9,24 +9,37 @@ import time
 # OSX locations /Users/<User>/Library/Android/sdk/...
 
 # Takes as argument the path to the Get-Notification apk file
-pathToAppApk = sys.argv[1]
+#pathToAppApk = sys.argv[1]
 # Form console command to list installed vms
 listAvds = ['emulator', '-list-avds']
 # Save result to variable
 # Everything below right now only works for single VM
 # TODO: Change to work with list of VMs
-avd = subprocess.Popen(listAvds, stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
-print(avd)
+avds = subprocess.Popen(listAvds, stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
+avdList = avds.split()
 
-# Launch VM(s)
-launch = ['emulator', '-avd', str(avd)]
-subprocess.Popen(launch, shell=True)
 
-# Give some time for VMs to launch
+# This seems to crash a number of the vms when launching like this
+for avd in avdList:
+    # Launch VM(s)
+    launch = ['emulator', '-avd', str(avd)]
+    subprocess.Popen(launch, shell=True)
+    time.sleep(60)
+
 time.sleep(30)
 
+listConnected = ['adb', 'devices']
+devices = subprocess.Popen(listConnected, stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
+
+print(devices)
+
+deviceList = devices.split()
+
+print(deviceList[0])
+# Give some time for VMs to launch
+#time.sleep(30)
 # Install the Get-Notification APK
-install = ['adb', 'install', pathToAppApk]
-subprocess.Popen(install, shell=True)
+#install = ['adb', 'install', pathToAppApk]
+#subprocess.Popen(install, shell=True)
 
 
--- monkeyrunnerTest.py
+++ monkeyrunnerTest.py
@@ -2,20 +2,32 @@
 ## monkeyrunner is apparently difficult when it comes to paths, so full paths are needed to files (I can show how it works in person on Wed.)
 ## the monkeyrunner.bat file actually has errors in it, and needs to be edited to even run without error
 
+
+## If you are editing this code in an IDE with autocomplete/linting/etc. the IDE probably wont find the com.android.monkeyrunner import and will give you warnings/errors
+## It does still run despite this, since it goes through the monkeyrunner application which interprets it using Jython instead (and it correctly resolves this import)
+## I'll look into if there's any way to get regular Python to recognize it
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
 device = MonkeyRunner.waitForConnection()
 
 device.installPackage('C:/Users/jthie/Desktop/SPARTALabRepos/Get-Notification/GetNotification/app/build/outputs/apk/debug/app-debug.apk')
 
-package = "com.example.MyGetNotificationApp"
+## IMPORTANT: Google/Android Devs' example is wrong: this is NOT supposed to be the package, it is supposed to be the ApplicationID (found in the build.gradle file) 
+package = "com.example.GetNotificationService"
 activity = "com.example.MyGetNotificationApp.MainActivity"
 
-runComponent = package + "/" + activity
+runComponent = package + '/' + activity
 
-## This line doesn't seem to actually launch anything?
+
 device.startActivity(component=runComponent)
 
 result = device.takeSnapshot()
 
 result.writeToFile("C:/Users/jthie/Desktop/SPARTALabRepos/test.png", "png")
+
+## Things we need to figure out:
+## This now installs and launches the notification listener
+## On startup, user is automatically redirected to notification privleges screen
+## We need to find out how to programatically find our app in the list and toggle the permissions to on
+## From there we can open browser and send the machines to visit pages
+## I know that there is a way to send psuedo random events/actions to the vms, but I'll need to see if that's even what we want
