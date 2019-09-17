beed5db1f269d794b5fdb8c4eac6683ed2a9566f
John Thiede <johnthiede94@gmail.com>
2019-09-17 09:13:35 -0500

    Updating script to work properly w/ multiple devices

--- triggerNotifications.py
+++ triggerNotifications.py
@@ -5,11 +5,13 @@ import itertools
 import re
 import json
 import telnetlib
+import datetime
 
 def getName(dev):
     port = str(dev).split('-')[1]
     HOST = 'localhost'
-    AUTH = 'NwhG3frGXDUGGYBz'
+    # AUTH = 'NwhG3frGXDUGGYBz'
+    AUTH = '555KjfyUBwIiO+h4'
     tel = telnetlib.Telnet(HOST, port)
     time.sleep(1)
     output = tel.read_very_eager()
@@ -69,44 +71,7 @@ def listConnected():
     filteredList = list(filter(lambda x : "emulator" in x, deviceList))
     return filteredList
 
-# inp = sys.argv[1]
-
-# with open(inp) as f:
-#     jsonList = json.loads(f.read())
-
-
-# stopList = listConnected()
-# for dev in stopList:
-#     stop = ['adb', '-s', str(dev), 'emu', 'kill']
-#     subprocess.Popen(stop).communicate()
-#     print("Waiting for state to save")
-#     while True:
-#         currentList = listConnected()
-#         if (not(dev in currentList)):
-#             break
-
-# print("State saved")
-# for device in jsonList:
-#     launch = ['emulator', '-avd', str(device['name']), '-noaudio', '-writable-system', '-http-proxy', '192.168.122.1:8890']
-#     subprocess.Popen(launch)
-
-
-
-filteredList = listConnected()
-print("Launched")
-for dev in filteredList:
-    
-    openStatusBar = ['adb', '-s', str(dev), 'shell', 'input', 'swipe', '500', '0', '500', '900']
-    home = ['adb', '-s', str(dev), 'shell', 'input', 'keyevent', 'KEYCODE_HOME']
-    bootStatus = ['adb', '-s', str(dev), 'shell', 'getprop', 'sys.boot_completed']
-
-    # print("Checking if booted")
-    # while True:
-    #     booted = subprocess.Popen(bootStatus, stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
-    #     time.sleep(1)
-    #     if booted.strip() == "1":
-    #         break
-    # print("Boot complete")
+def triggerNotifs(dev):
     while(True):
         # Here's one cycle of targeting a notif.
         # Have to repeat this for each non system notification
@@ -133,7 +98,123 @@ for dev in filteredList:
                 break
             elif (not("id/expand_button" in dumpOut[i]) and i == len(dumpOut) - 1):
                 subprocess.Popen(home).communicate()
-                exit()
+
+               
+                return
+
+
+    return
+
+inp = sys.argv[1]
+
+date = str(datetime.date.today())
+
+with open(inp) as f:
+    jsonList = json.loads(f.read())
+
+
+stopList = listConnected()
+for dev in stopList:
+    stop = ['adb', '-s', str(dev), 'emu', 'kill']
+    subprocess.Popen(stop).communicate()
+    print("Waiting for state to save")
+    while True:
+        currentList = listConnected()
+        if (not(dev in currentList)):
+            break
+
+print("State saved")
+for device in jsonList:
+    launch = ['emulator', '-avd', str(device['name']), '-noaudio', '-writable-system', '-http-proxy', '192.168.122.1:8890']
+    subprocess.Popen(launch)
+
+
+
+time.sleep(5)
+filteredList = listConnected()
+print("Launched")
+for dev in filteredList:
+    
+
+
+    openStatusBar = ['adb', '-s', str(dev), 'shell', 'input', 'swipe', '500', '0', '500', '900']
+    home = ['adb', '-s', str(dev), 'shell', 'input', 'keyevent', 'KEYCODE_HOME']
+    bootStatus = ['adb', '-s', str(dev), 'shell', 'getprop', 'sys.boot_completed']
+    
+    exit = ['adb', '-s', str(dev), 'emu', 'kill']
+    
+
+    
+    time.sleep(2)
+    print("Checking if booted")
+    while True:
+        booted = subprocess.Popen(bootStatus, stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
+        time.sleep(1)
+        if booted.strip() == "1":
+            break
+    print("Boot complete")
+
+    devName = getName(dev)
+    print(devName)
+    mitmdump = ['mitmdump', '--listen-host', '192.168.122.1', '--listen-port', '8890', '-w', devName + '_' + date + '.cap']
+    relaunch = ['emulator', '-avd', devName, '-noaudio', '-writable-system']
+    proxy = subprocess.Popen(mitmdump)
+
+
+    triggerNotifs(dev)
+
+    ## TODO: Move this to outside of loop?
+    ## Sometimes saving state takes minutes to actually complete
+    ## Terminate proxy as before, but close/relaunch machines afterwards
+    subprocess.Popen(exit).communicate()
+    print("Waiting for state to save")
+    while True:
+        currentList = listConnected()
+        if (not(dev in currentList)):
+            break
+                
+    proxy.terminate()
+    subprocess.Popen(relaunch)
+
+    ## TODO: Make this a function
+    ## When done, return instead of exit
+    # while(True):
+    #     # Here's one cycle of targeting a notif.
+    #     # Have to repeat this for each non system notification
+    #     subprocess.Popen(openStatusBar).communicate()
+    #     time.sleep(3)
+    #     dumpOut = dump(dev)
+        
+    #     for i in range(0, len(dumpOut)):
+    #         # print(str(line.encode('ascii', 'ignore')))
+    #         if ("id/expand_button" in dumpOut[i]):
+    #             # print(str(dumpOut[i].encode('ascii', 'ignore')))
+    #             tapElement(dumpOut[i], str(dev))
+    #             dumpOut2 = dump(dev)
+    #             for line in dumpOut2:
+    #                 if(("id/title" in line or "id/expanded_notification_title" in line) and not(("Android Setup" in line) or ("Preparing for setup" in line))):
+    #                     # print(line.encode('ascii', 'ignore'))
+    #                     tapElement(line, str(dev))
+    #                     time.sleep(10)   
+    #                     subprocess.Popen(home).communicate()
+    #                     time.sleep(2)
+    #                     dismissApp(str(dev))
+    #                     time.sleep(2)
+    #                     break
+    #             break
+    #         elif (not("id/expand_button" in dumpOut[i]) and i == len(dumpOut) - 1):
+    #             subprocess.Popen(home).communicate()
+
+    #             subprocess.Popen(exit).communicate()
+    #             print("Waiting for state to save")
+    #             while True:
+    #                 currentList = listConnected()
+    #                 if (not(dev in currentList)):
+    #                     break
+                
+    #             proxy.kill()
+    #             subprocess.Popen(relaunch)
+    #             break
 
         
         
